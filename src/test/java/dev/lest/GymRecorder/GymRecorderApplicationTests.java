448a07e599c0506945649152fdf9c407276972fc
Leonardo Figorelli Santana <leofigorelli@gmail.com>
2025-07-01 08:14:19 -0300

    first commit

---
+++ src/test/java/dev/lest/GymRecorder/GymRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lest.GymRecorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
