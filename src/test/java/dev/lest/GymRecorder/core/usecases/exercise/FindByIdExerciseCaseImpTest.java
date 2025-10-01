88ae8f533e053c946e81cb563072b8849c2b1f2d
Leonardo Figorelli Santana <leofigorelli@gmail.com>
2025-09-30 22:23:46 -0300

    Test: Cria teste unitarios para as buscas de entidades

--- src/test/java/dev/lest/GymRecorder/core/usecases/exercise/FindByIdExerciseCaseImpTest.java
+++ src/test/java/dev/lest/GymRecorder/core/usecases/exercise/FindByIdExerciseCaseImpTest.java
@@ -1,13 +1,24 @@
 package dev.lest.GymRecorder.core.usecases.exercise;
 
+import dev.lest.GymRecorder.core.entities.Exercise;
+import dev.lest.GymRecorder.core.entities.User;
 import dev.lest.GymRecorder.core.gateway.ExerciseGateway;
 import dev.lest.GymRecorder.core.usecases.Exercise.classes.FindByIdExerciseCaseImp;
+import dev.lest.GymRecorder.infrastructure.exception.Exercise.ExerciseNotFoundException;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
@@ -19,6 +30,39 @@ public class FindByIdExerciseCaseImpTest {
     @InjectMocks
     private FindByIdExerciseCaseImp exerciseCaseImp;
 
-    public void shouldExecuteOnSuccess() {}
+    @Test
+    public void shouldExecuteOnSuccess() {
+        String exerciseId = "507f191e810c19729de860ea";
+        Exercise expectExercise = new Exercise(
+                exerciseId,
+                "Supino Reto",
+                12,
+                4,
+                120.0,
+                Duration.ofMinutes(2L),
+                new User()
+        );
+
+        when(exerciseGateway.findExerciseById(exerciseId)).thenReturn(Optional.of(expectExercise));
+
+        Exercise exercise = exerciseCaseImp.execute(exerciseId);
+        assertThat(exercise).isNotNull();
+        assertThat(exercise).isEqualTo(expectExercise);
+
+        verify(exerciseGateway, times(1)).findExerciseById(exerciseId);
+    }
+
+    @Test
+    public void sholdExecuteOnFailure() {
+        String nonExistentExerciseId = "A1B2C3D4E5F6G7H8I9J1K2L3";
+
+        when(exerciseGateway.findExerciseById(nonExistentExerciseId)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> exerciseCaseImp.execute(nonExistentExerciseId))
+                .isInstanceOf(ExerciseNotFoundException.class)
+                .hasMessage("Exercise Error: Exercise not exist!");
+
+    }
+
 
 }
--- src/test/java/dev/lest/GymRecorder/core/usecases/training/FindByIdTrainingCaseImpTest.java
+++ src/test/java/dev/lest/GymRecorder/core/usecases/training/FindByIdTrainingCaseImpTest.java
@@ -1,13 +1,27 @@
 package dev.lest.GymRecorder.core.usecases.training;
 
+import dev.lest.GymRecorder.core.entities.Exercise;
+import dev.lest.GymRecorder.core.entities.Training;
+import dev.lest.GymRecorder.core.entities.User;
+import dev.lest.GymRecorder.core.enuns.DayWeek;
 import dev.lest.GymRecorder.core.gateway.TrainingGateway;
 import dev.lest.GymRecorder.core.usecases.Training.classes.FindByIdTrainingCaseImp;
+import dev.lest.GymRecorder.infrastructure.exception.Training.TrainingNotFoundException;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
@@ -19,5 +33,38 @@ public class FindByIdTrainingCaseImpTest {
     @InjectMocks
     private FindByIdTrainingCaseImp trainingCaseImp;
 
-    public void shouldExecuteOnSuccess() {}
+    @Test
+    public void shouldExecuteOnSuccess() {
+        String trainingId = "507f191e810c19729de860ea";
+        Training expectedTraining = new Training(
+            trainingId,
+            "Treino de Peito",
+            DayWeek.MONDAY,
+            List.of(
+                    new Exercise(),
+                    new Exercise()),
+            Duration.ofMinutes(120L),
+            new User()
+        );
+
+        when(trainingGateway.findTrainingById(trainingId)).thenReturn(Optional.of(expectedTraining));
+
+        Training training = trainingCaseImp.execute(trainingId);
+        assertThat(training).isNotNull();
+        assertThat(training).isEqualTo(expectedTraining);
+
+        verify(trainingGateway, times(1)).findTrainingById(trainingId);
+    }
+
+    @Test
+    public void shouldExecuteOnFailure() {
+        String nonExistentExerciseId = "507f191e810c19729de860ea";
+
+        when(trainingGateway.findTrainingById(nonExistentExerciseId)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> trainingCaseImp.execute(nonExistentExerciseId))
+                .isInstanceOf(TrainingNotFoundException.class)
+                .hasMessage("Training not exist");
+
+    }
 }
